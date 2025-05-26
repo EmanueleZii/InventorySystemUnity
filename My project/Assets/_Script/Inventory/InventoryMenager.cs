8c4772f09a11f0a1b953ae4a2c8ce0f2efec1b2b
Emanuele Zignale <zignale92@gmail.com>
2025-05-26 19:04:23 +0200

    update code
    semplificazione di una ricerca di item nel inventario piu facile da leggere

--- My project/Assets/_Script/Inventory/InventoryMenager.cs
+++ My project/Assets/_Script/Inventory/InventoryMenager.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class InventoryMenager : MonoBehaviour
-{
+public class InventoryMenager : MonoBehaviour {
     public List<InventoryItem> inventoryItems;
     public GameObject zaino;
     private bool isInventoryOpen = false;
     Item newItem;
     public GameObject inventoryItemPrefab;
     public Transform inventoryContent;
-    public InventoryItem GetItemByType<T>() where T : Item
-    {
-        return inventoryItems.Find(i => i.itemData is T);
-    }
 
+    //cerca sul inventario un oggetto che contiene itemData 
+     public InventoryItem GetItemByType<T>() where T : Item
+    {
+        foreach (var item in inventoryItems)
+        {
+            if (item.itemData is T)
+                return item;
+        }
+
+        return null; // Nessun item trovato
+    }
+    
     // Questo metodo viene richiamato dal Button UI
     public void ToggleInventory()
     {
@@ -38,7 +46,7 @@ public class InventoryMenager : MonoBehaviour
         itemComponent.stackCount = 1;
 
         // Setta icona, se ce
-        var img = newItemGO.GetComponent<UnityEngine.UI.Image>();
+        var img = newItemGO.GetComponent<Image>();
         if (img != null && newItem.icon != null)
             img.sprite = newItem.icon;
 
